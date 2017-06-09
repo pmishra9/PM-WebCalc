3baf6771282ca83d363bc1af617f326dac4f36c1
pmishra <pmishra9@gmail.com>
2017-06-09 14:02:45 -0400

    @app.route('/<int:a>/<op>/<int:b>') @app.route('/<int:a>/<op>/<int:b>') @app.route('/<int:a>/<op>/<int:b>') def calc(a, op, b):     operation = mongo.db.operations.find_one({'name': op})     if operation:         return Template(operation['pattern']).render(a=a, b=b)     elif op == '+':         return f"Result: {a} {op} {b} = {a + b}"     else:         return f"Result: {a} {op} {b} = ???" def calc(a, op, b):     operation = mongo.db.operations.find_one({'name': op})     if operation:         return Template(operation['pattern']).render(a=a, b=b)     elif op == '+':         return f"Result: {a} {op} {b} = {a + b}"     else:         return f"Result: {a} {op} {b} = ???" def calc(a, op, b):     operation = mongo.db.operations.find_one({'name': op})     if operation:         return Template(operation['pattern']).render(a=a, b=b)     elif op == '+':         return f"Result: {a} {op} {b} = {a + b}"     else:         return f"Result: {a} {op} {b} = ???"

--- test_webcalc.py
+++ test_webcalc.py
@@ -22,3 +22,22 @@ def describe_calc():
             response = client.get('/4/+/5')
 
             expect(response.data).contains(b"9")
+
+@pytest.fixture
+def pattern():
+    with app.app_context():
+        mongo.db.operations.drop()
+        mongo.db.operations.insert(
+            dict(
+                name="x",
+                pattern="{{ a * b }}"
+            )
+        )
+…
+
+def describe_calc():
+        …
+        def from_db(client, pattern):
+            response = client.get('/4/x/5')
+
+            expect(response.data).contains(b"20")
--- webcalc.py
+++ webcalc.py
@@ -14,4 +14,24 @@ if __name__ == '__main__':
 
 @app.route('/<int:a>/<op>/<int:b>')
 def calc(a, op, b):
-    return f"Result: {a} {op} {b} = {a + b}"
+    operation = mongo.db.operations.find_one({'name': op})
+    if operation:
+        return Template(operation['pattern']).render(a=a, b=b)
+    elif op == '+':
+        return f"Result: {a} {op} {b} = {a + b}"
+    else:
+        return f"Result: {a} {op} {b} = ???"
+
+
+import os
+
+from flask import Flask
+from flask_pymongo import PyMongo
+from jinja2 import Template
+
+app = Flask(__name__)
+
+if os.getenv('MONGODB_URI'):
+    app.config['MONGO_URI'] = os.getenv('MONGODB_URI')
+
+mongo = PyMongo(app)
